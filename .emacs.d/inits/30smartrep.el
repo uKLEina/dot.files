4291d8713100a7f3aeada6a87c8178334e9f359f
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2017-03-02 09:07:09 +0900

    smartrep.elを追加

--- .emacs.d/inits/30smartrep.el
+++ .emacs.d/inits/30smartrep.el
@@ -1,5 +1,11 @@
 (require 'smartrep)
+(require 'python-mode)
+
 (smartrep-define-key
     python-mode-map "C-c"
   '((">" . py-shift-right)
     ("<" . py-shift-left)))
+
+(smartrep-define-key
+    global-map "C-l"
+  '(("TAB" . tab-to-tab-stop)))
