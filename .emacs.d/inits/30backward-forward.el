052aaf9da6133a07b36a3a43f76b4e54e739dab8
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2017-03-14 14:46:05 +0900

    Gとかggした後にbackward-forwardで戻れるようにした

--- .emacs.d/inits/30backward-forward.el
+++ .emacs.d/inits/30backward-forward.el
@@ -3,4 +3,6 @@
 (setq backward-forward-evil-compatibility-mode t)
 
 (define-key backward-forward-mode-map (kbd "C-l C-a") 'backward-forward-previous-location)
-(define-key backward-forward-mode-map (kbd "C-l C-f") 'backward-forward-previous-location)
+(define-key backward-forward-mode-map (kbd "C-l C-f") 'backward-forward-next-location)
+(advice-add 'evil-goto-first-line :before #'backward-forward-push-mark-wrapper)
+(advice-add 'evil-goto-line :before #'backward-forward-push-mark-wrapper)
