77d9c4b221de7390f18c6cac4b99d3f52d5ae961
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2017-03-23 17:35:31 +0900

    pyflakes消した(デフォでflake8とpylintに対応してた)

--- .emacs.d/inits/50python.el
+++ .emacs.d/inits/50python.el
@@ -1,11 +1,10 @@
 (require 'python-mode)
 
 ;;; IDEてきなやついろいろ
-(require 'flycheck-pyflakes)
 (add-hook 'python-mode-hook 'flycheck-mode)
 
 (require 'py-autopep8)
-(setq py-autopep8-options '("--max-line-length=1000"))
+(setq py-autopep8-options '("--max-line-length=100"))
 (add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
 
 (require 'jedi)
