f8d1195859cbe7293ba1186722268b0fa35eafb3
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2017-03-30 12:58:52 +0900

    pylintrcを追加

--- .emacs.d/inits/30smartrep.el
+++ .emacs.d/inits/30smartrep.el
@@ -9,3 +9,8 @@
 (smartrep-define-key
     global-map "C-l"
   '(("TAB" . tab-to-tab-stop)))
+
+(smartrep-define-key
+    flycheck-mode-map "C-c !"
+  '(("n" . flycheck-next-error)
+    ("p" . flycheck-previous-error)))
