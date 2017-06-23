b1839080cfe895a7aa74688811b8e5d6fe46411d
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2017-06-23 17:49:44 +0900

    python imenu fix & linux path fix

--- .emacs.d/inits/linux-path.el
+++ .emacs.d/inits/linux-path.el
@@ -1,2 +1,3 @@
+(require 'exec-path-from-shell)
 (setq exec-path-from-shell-check-startup-files nil)
 (exec-path-from-shell-initialize)
--- .emacs.d/inits/linux-python.el
+++ .emacs.d/inits/linux-python.el
@@ -1,8 +1,9 @@
 (require 'flycheck)
 (require 'imenu)
+(require 'python-mode)
 (setq flycheck-flake8-maximum-line-length 200)
 (flycheck-add-next-checker 'python-flake8 'python-pylint)
-(semantic-mode 1)
 (add-hook 'python-mode-hook
   (lambda ()
-    (setq imenu-create-index-function 'python-imenu-create-index)))
+    (setq imenu-create-index-function 'py--imenu-create-index-new)
+    (setq py--imenu-create-index-p t)))
