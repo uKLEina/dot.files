92dfc1f00c156da3fc1cf695cc580c31208d9cd9
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2017-06-23 18:05:35 +0900

    jedi用にPYTHONPATHを使えるように変更

--- .emacs.d/inits/50python.el
+++ .emacs.d/inits/50python.el
@@ -8,19 +8,9 @@
 (add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
 
 (require 'jedi)
-;; きちゃないけどjedi用の環境を直接書いちゃう
-(setq jedi:environment-root "~/.virtualenvs/tanawari")
+;;; jediの補完はPYTHONPATHを見てくれる
 (setq jedi:complete-on-dot t)
 (define-key python-mode-map (kbd "C-M-i") 'jedi:complete)
-;; pathも直接書いちゃう
-(add-hook 'python-mode-hook
-          (lambda ()
-            (cond ((file-exists-p "~/.virtualenvs/tanawari/Scripts")
-                   (setq exec-path (append '("~/.virtualenvs/tanawari/Scripts") exec-path))
-                   (setenv "PATH" (concat "~/.virtualenvs/tanawari/Scripts:" (getenv "PATH"))))
-                  ((file-exists-p "~/.virtualenvs/tanawari/bin")
-                   (setq exec-path (append '("~/.virtualenvs/tanawari/bin") exec-path))
-                   (setenv "PATH" (concat "~/.virtualenvs/tanawari/bin:" (getenv "PATH")))))))
 (add-hook 'python-mode-hook 'jedi:setup)
 (add-hook 'python-mode-hook
           (lambda ()
--- .emacs.d/inits/linux-path.el
+++ .emacs.d/inits/linux-path.el
@@ -1,3 +1,3 @@
 (require 'exec-path-from-shell)
 (setq exec-path-from-shell-check-startup-files nil)
-(exec-path-from-shell-initialize)
+(exec-path-from-shell-copy-envs '("PATH" "MANPATH" "PYTHONPATH"))
