6918a053b2bc2d2677bbaa26fa7f195b1fc82c2d
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2017-06-26 09:13:59 +0900

    package update

--- .emacs.d/elisp/resize-window-20170329.1644/resize-window-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "resize-window" "20170329.1644" "easily resize windows" '((emacs "24") (cl-lib "0.5")) :url "https://github.com/dpsutton/resize-mode" :keywords '("window" "resize"))
---
+++ .emacs.d/elisp/resize-window-20170624.1658/resize-window-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "resize-window" "20170624.1658" "easily resize windows" '((emacs "24") (cl-lib "0.5")) :commit "274a96e9754b606d85c9dd62cfed628ff4f736df" :url "https://github.com/dpsutton/resize-mode" :keywords '("window" "resize"))
