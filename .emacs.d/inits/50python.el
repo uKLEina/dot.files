a8cd1a690247d6bc4ab48035a182e36fa409dc70
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2017-06-27 09:10:14 +0900

    virtualenvwrapper追加

--- .emacs.d/inits/50python.el
+++ .emacs.d/inits/50python.el
@@ -1,14 +1,22 @@
 (require 'python-mode)
+(setq auto-mode-alist (cons '("\\.py\\'" . python-mode) auto-mode-alist))
+
+;;; virtualenv
+(require 'virtualenvwrapper)
+;; (require 'auto-virtualenvwrapper)
+;; (add-hook 'python-mode-hook #'auto-virtualenvwrapper-activate)
+;; (add-hook 'projectile-after-switch-project-hook #'auto-virtualenvwrapper-activate)
 
 ;;; IDEてきなやついろいろ
 (add-hook 'python-mode-hook 'flycheck-mode)
+(add-hook 'python-mode-hook #'smartparens-mode)
 
 (require 'py-autopep8)
 (setq py-autopep8-options '("--max-line-length=200"))
 (add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
 
 (require 'jedi)
-;;; jediの補完はPYTHONPATHを見てくれる
+;;; jediの補完はPYTHONPATHを見てくれるらしい
 (setq jedi:complete-on-dot t)
 (define-key python-mode-map (kbd "C-M-i") 'jedi:complete)
 (add-hook 'python-mode-hook 'jedi:setup)
@@ -26,10 +34,11 @@
 (require 'evil)
 (add-hook 'python-mode-hook
           (lambda ()
-            (evil-make-intercept-map jedi-mode-map)))
+            (evil-make-intercept-map jedi-mode-map)
+            ;; (setenv "PATH" (concat (getenv "PYTHONPATH") ":" (getenv "PATH")))
+            ;; (exec-path-from-shell-copy-envs '("PATH" "VIRTUALENVWRAPPER_PYTHON" "WORKON_HOME" "PYTHONPATH"))
+            ))
 (setq jedi:use-shortcuts t)
-
-(add-hook 'python-mode-hook #'smartparens-mode)
 
 ;;; workaround
 (with-eval-after-load 'python
