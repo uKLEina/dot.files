c525e26c2f2e228095fd5f5e34e2a46a0e1415a0
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2017-07-18 09:56:30 +0900

    aspell 強化

---
+++ .emacs.d/elisp/flyspell-correct-helm-0.4/flyspell-correct-helm-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flyspell-correct-helm" "0.4" "correcting words with flyspell via helm interface" '((flyspell-correct "0.4.0") (helm "1.9.0")) :commit "7e7f94a36699c7e7bba728df722e13a7b4af4b73" :url "https://github.com/d12frosted/flyspell-correct")
--- .emacs.d/inits/linux-10aspell.el
+++ .emacs.d/inits/linux-10aspell.el
@@ -1,5 +1,27 @@
 (use-package ispell
   :defer t
+  :init
+  (mapc
+   (lambda (hook)
+     (add-hook hook 'flyspell-prog-mode))
+   '(c++-mode-hook
+     emacs-lisp-mode-hook
+     python-mode-hook
+     js2-mode-hook
+     tuareg-mode-hook
+     java-mode-hook
+     php-mode-hook))
   :config
   (setq-default ispell-program-name "aspell")
-  (add-to-list 'ispell-skip-region-alist '("[^\000-\377]+")))
+  (add-to-list 'ispell-skip-region-alist '("[^\000-\377]+"))
+  (ac-ispell-setup))
+
+(use-package ac-ispell
+  :defer t)
+
+(use-package flyspell-correct
+  :defer t
+  :init
+  (bind-key "C-M-;" 'flyspell-correct-word-generic)
+  :config
+  (use-package flyspell-correct-helm))
