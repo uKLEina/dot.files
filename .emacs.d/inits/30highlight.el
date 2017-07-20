a23592b0808dc1b5fceca8880d42b9159d10b060
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2017-07-20 17:28:46 +0900

    update highlight setting

--- .emacs.d/inits/30highlight.el
+++ .emacs.d/inits/30highlight.el
@@ -1,5 +1,8 @@
 (use-package highlight-symbol
+  :defer t
+  :init
+  (add-hook 'prog-mode-hook 'highlight-symbol-mode)
+  (add-hook 'prog-mode-hook 'highlight-symbol-nav-mode)
   :config
-  (setq highlight-symbol-idle-delay 0.75)
-  :bind (("M-#" . highlight-symbol-query-replace)
-         ("C-l C-s" . highlight-symbol)))
+  (setq highlight-symbol-idle-delay 0.5)
+  (bind-key "C-l C-s" 'highlight-symbol))
