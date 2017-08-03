59597bec802b933d5c0e86babaeabdb63c9c20e4
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2017-08-03 15:34:20 +0900

    git-commit-modeの初期evil-stateがnormal-stateになるように変更

--- .emacs.d/inits/40git.el
+++ .emacs.d/inits/40git.el
@@ -1,2 +1,4 @@
 (use-package git-commit
-  :mode (("COMMIT_EDITMSG" . git-commit-mode)))
+  :mode (("COMMIT_EDITMSG" . git-commit-mode))
+  :init (evil-set-initial-state 'git-commit-mode 'normal)
+  )
