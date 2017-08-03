5730c9dad8bca413ad5a476d4f2b41f035b0af87
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2017-08-03 15:48:09 +0900

    package install: switch-buffer-functions

---
+++ .emacs.d/elisp/switch-buffer-functions-0.0.1/switch-buffer-functions-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "switch-buffer-functions" "0.0.1" "Hook run when current buffer changed" 'nil :commit "e1bccfff2d123b6218efab16c486215cedb9a108" :url "https://github.com/10sr/switch-buffer-functions-el" :keywords '("hook" "utility"))
