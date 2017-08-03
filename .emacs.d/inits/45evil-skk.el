fcf7b3448d707aafb777a2edf3d5c9b1060ef35b
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2017-08-03 15:30:17 +0900

    evil-skkの設定を削除

--- .emacs.d/inits/45evil-skk.el
+++ .emacs.d/inits/45evil-skk.el
@@ -1,14 +1,14 @@
-(defadvice update-buffer-local-cursor-color
-  (around evil-update-buffer-local-cursor-color-in-insert-state activate)
-  ;; SKKによるカーソル色変更を, 挿入ステートかつ日本語モードの場合に限定
-  "Allow ccc to update cursor color only when we are in insert
-state and in `skk-j-mode'."
-  (when (and (eq evil-state 'insert) (bound-and-true-p skk-j-mode))
-    ad-do-it))
-(defadvice evil-refresh-cursor
-  (around evil-refresh-cursor-unless-skk-mode activate)
-  ;; Evilによるカーソルの変更を, 挿入ステートかつ日本語モードではない場合に限定
-  "Allow ccc to update cursor color only when we are in insert
-state and in `skk-j-mode'."
-  (unless (and (eq evil-state 'insert) (bound-and-true-p skk-j-mode))
-    ad-do-it))
+;; (defadvice update-buffer-local-cursor-color
+;;   (around evil-update-buffer-local-cursor-color-in-insert-state activate)
+;;   ;; SKKによるカーソル色変更を, 挿入ステートかつ日本語モードの場合に限定
+;;   "Allow ccc to update cursor color only when we are in insert
+;; state and in `skk-j-mode'."
+;;   (when (and (eq evil-state 'insert) (bound-and-true-p skk-j-mode))
+;;     ad-do-it))
+;; (defadvice evil-refresh-cursor
+;;   (around evil-refresh-cursor-unless-skk-mode activate)
+;;   ;; Evilによるカーソルの変更を, 挿入ステートかつ日本語モードではない場合に限定
+;;   "Allow ccc to update cursor color only when we are in insert
+;; state and in `skk-j-mode'."
+;;   (unless (and (eq evil-state 'insert) (bound-and-true-p skk-j-mode))
+;;     ad-do-it))
