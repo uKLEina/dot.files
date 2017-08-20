e3c101b506e8a75feb44155217844d85abf3a4e1
uKLEina <xopilli@gmail.com>
2017-08-20 20:30:53 +0900

    package update

--- .emacs.d/elisp/plantuml-mode-1.2.3/plantuml-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "plantuml-mode" "1.2.3" "Major mode for PlantUML" '((emacs "24")) :commit "eb380bac5663ec8e73482db6c6ba9d1394766eb3" :keywords '("uml" "plantuml" "ascii"))
--- .emacs.d/elisp/vimrc-mode-20150607.913/vimrc-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "vimrc-mode" "20150607.913" "Major mode for vimrc files" 'nil :commit "fae7604e5ea6eef104d8c68598c3bbf9d798aa74" :url "https://github.com/mcandre/vimrc-mode" :keywords '("languages" "vim"))
