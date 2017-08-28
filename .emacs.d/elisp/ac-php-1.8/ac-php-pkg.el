9e80e35b5ce3d8a17bbe93d15e15dd2d703b9373
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2017-08-28 09:18:36 +0900

    package update

---
+++ .emacs.d/elisp/ac-php-1.8/ac-php-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ac-php" "1.8" "auto-completion source for php" '((ac-php-core "1") (auto-complete "1.4.0") (yasnippet "0.8.0")) :commit "7a50b7debd149a30b84f0e2e57a4662926d96a24" :url "https://github.com/xcwen/ac-php" :keywords '("completion" "convenience" "intellisense"))
