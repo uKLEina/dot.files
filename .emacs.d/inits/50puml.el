ff6f2cb63aa1f2033edbc3e5e5e9f3a941b689f2
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2017-09-01 16:34:27 +0900

    puml-modeで保存時に生成画像を自動ポップアップしないように変更

--- .emacs.d/inits/50puml.el
+++ .emacs.d/inits/50puml.el
@@ -4,12 +4,12 @@
          ("\\.puml\\'" . plantuml-mode)
          ("\\.pu\\'" . plantuml-mode))
   :config
-  (custom-set-variables '(plantuml-jar-path "/usr/share/plantuml/plantuml.jar"))
-  (add-hook 'plantuml-mode-hook
-            (lambda ()
-              (add-hook 'after-save-hook
-                        (lambda ()
-                          (plantuml-preview-buffer 4)) nil 'make-it-local)))
+  (custom-set-variables '(plantuml-jar-path "~/plantuml.jar"))
+  ;; (add-hook 'plantuml-mode-hook
+  ;;           (lambda ()
+  ;;             (add-hook 'after-save-hook
+  ;;                       (lambda ()
+  ;;                         (plantuml-preview-buffer 4)) nil 'make-it-local)))
   (push '("*PLANTUML Preview*" :position right :width 50 :noselect t)
         popwin:special-display-config)
   )
