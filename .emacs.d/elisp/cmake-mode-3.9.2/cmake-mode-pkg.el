26a68e496552b13fee03c0e11545f335012cc7f0
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2017-09-14 09:31:03 +0900

    package update

---
+++ .emacs.d/elisp/cmake-mode-3.9.2/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "3.9.2" "major-mode for editing CMake sources" 'nil :commit "ffa164736ec78f27583ca4515ff3dfc7a82fc702")
--- .emacs.d/elisp/packed-2.0.1/packed-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "packed" "2.0.1" "package manager agnostic Emacs Lisp package utilities" '((emacs "24.3") (dash "2.12.1")) :commit "536f4a3bda06cc09759fed1aa0cdebb068ff75a1" :url "https://github.com/tarsius/packed" :keywords '("compile" "convenience" "lisp" "package" "library"))
