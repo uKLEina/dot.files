23fcd5d4bb26e48c98ec81169c429b789a5da65b
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2017-09-21 16:29:27 +0900

    package update

--- .emacs.d/elisp/cython-mode-0.26.2.2/cython-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "cython-mode" "0.26.2.2" "Major mode for editing Cython files" 'nil :commit "3ea17b707dde432c7b7b155aa227851eb2ef3eab")
---
+++ .emacs.d/elisp/cython-mode-0.27.2.1/cython-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cython-mode" "0.27.2.1" "Major mode for editing Cython files" 'nil :commit "c84e36b8e133c7795a8d10fbee83efb20146a969")
