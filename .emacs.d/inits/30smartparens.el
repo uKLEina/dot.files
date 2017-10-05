debcf54b842d1ebd9609c82d9fab2179006bca64
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2017-10-05 14:25:18 +0900

    elisp-mode時に`'がsmartparensされないように変更

--- .emacs.d/inits/30smartparens.el
+++ .emacs.d/inits/30smartparens.el
@@ -5,4 +5,8 @@
   (add-hook 'python-mode-hook #'smartparens-mode)
   (add-hook 'c-mode-hook #'smartparens-mode)
   (add-hook 'c++-mode-hook #'smartparens-mode)
-  (add-hook 'java-mode-hook #'smartparens-mode))
+  (add-hook 'java-mode-hook #'smartparens-mode)
+  :config
+  (sp-local-pair 'emacs-lisp-mode "`" nil :acitons nil)
+  (sp-local-pair 'emacs-lisp-mode "'" nil :actions nil)
+  )
