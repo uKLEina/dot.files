1ba4d3854152ac9b0ea5d7fba1fadefb06a675a0
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2017-10-18 09:17:56 +0900

    emacs package update

---
+++ .emacs.d/elisp/caml-4.6.0beta2/caml-pkg.el
@@ -0,0 +1,5 @@
+(define-package "caml" "4.6.0beta2" "OCaml code editing commands for Emacs" 'nil :url "https://github.com/ocaml/ocaml/" :keywords
+  '("ocaml"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- .emacs.d/elisp/tuareg-2.0.10/tuareg-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "tuareg" "2.0.10" "No description available." 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
