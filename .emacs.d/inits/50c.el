d85c109c8df67554d45c200f671bb86176dfdf2a
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2017-12-08 11:46:27 +0900

    c/c++用の設定をc-common系を使わないように変更(PHP/Java対策)

--- .emacs.d/inits/50c.el
+++ .emacs.d/inits/50c.el
@@ -1,25 +1,22 @@
 (use-package rtags
   :load-path "/usr/local/share/emacs/site-lisp/rtags/"
   :init
-  (add-hook 'c-mode-hook 'rtags-start-process-unless-running)
-  (add-hook 'c++-mode-hook 'rtags-start-process-unless-running)
-  (add-hook 'objc-mode-hook 'rtags-start-process-unless-running)
-  (defun my-flycheck-rtags-setup ()
+  (defun c/c++-mode-setup ()
+    "hook function for `c-mode' and `c++-mode'."
+    (rtags-start-process-unless-running)
     (flycheck-select-checker 'rtags)
     (setq-local flycheck-highlighting-mode nil) ;; RTags creates more accurate overlays.
-    (setq-local flycheck-check-syntax-automatically nil))
-  (add-hook 'c-mode-common-hook #'my-flycheck-rtags-setup)
-  (add-hook 'c-mode-common-hook
-            (lambda ()
-              (when (rtags-is-indexed)
-                (local-set-key (kbd "M-.") 'rtags-find-symbol-at-point)
-                (local-set-key (kbd "M-;") 'rtags-find-symbol)
-                (local-set-key (kbd "M-@") 'rtags-find-references)
-                (local-set-key (kbd "M-,") 'rtags-location-stack-back))))
+    (setq-local flycheck-check-syntax-automatically nil)
+    (when (rtags-is-indexed)
+      (local-set-key (kbd "M-.") 'rtags-find-symbol-at-point)
+      (local-set-key (kbd "M-;") 'rtags-find-symbol)
+      (local-set-key (kbd "M-@") 'rtags-find-references)
+      (local-set-key (kbd "M-,") 'rtags-location-stack-back)))
+  (add-hook 'c-mode-hook #'c/c++-mode-setup)
+  (add-hook 'c++-mode-hook #'c/c++-mode-setup)
   :config
   (company-mode-on)
   (auto-complete-mode -1)
-  (evil-make-overriding-map c-mode-base-map)
   (evil-make-overriding-map c++-mode-map)
   (evil-make-overriding-map c-mode-map)
   (custom-set-variables
@@ -29,8 +26,7 @@
   (use-package company-rtags)
   (eval-after-load 'company
     '(add-to-list 'company-backends 'company-rtags))
-  (use-package flycheck-rtags)
-  )
+  (use-package flycheck-rtags))
 
 (use-package irony
   :defer t
@@ -44,8 +40,7 @@
   (use-package company-irony)
   (company-irony-setup-begin-commands)
   (use-package company-irony-c-headers)
-  (eval-after-load 'company
-    '(add-to-list 'company-backends '(company-irony-c-headers company-irony) ))
+  (add-to-list 'company-backends '(company-irony-c-headers company-irony))
   (use-package flycheck-irony)
-  (eval-after-load 'flycheck
-    '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup)))
+  (add-hook 'flycheck-mode-hook #'flycheck-irony-setup)
+  )
