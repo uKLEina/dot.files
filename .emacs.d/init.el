3768d9420b12e0c15b156f4871a9c6e87578752c
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2018-05-08 09:30:39 +0900

    zoom-frmとそのキーバインド設定を追加

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -99,6 +99,7 @@
   (el-get-bundle magit))
 (el-get-bundle evil-mc :type git :url "https://github.com/gabesoft/evil-mc.git" :depends (evil))
 (el-get-bundle visual-regexp-steroids)
+(el-get-bundle zoom-frm :depends (frame-cmds))
 
 
 ;; C/C++
@@ -120,6 +121,10 @@
 
 ;; JS
 (el-get-bundle js2-mode)
+
+;; Kotlin
+(el-get-bundle kotlin-mode)
+(el-get-bundle flycheck-kotlin :type git :url "https://github.com/whirm/flycheck-kotlin.git" :depends flycheck)
 
 ;; other modes
 (el-get-bundle meghanada :type git :url "https://github.com/mopemope/meghanada-emacs.git" :depends (yasnippet company-mode flycheck))
---
+++ .emacs.d/inits/20zoom-frm.el
@@ -0,0 +1,8 @@
+(use-package zoom-frame
+  :defer t
+  :init
+  (define-key ctl-x-map [(control ?+)] 'zoom-in/out)
+  (define-key ctl-x-map [(control ?-)] 'zoom-in/out)
+  (define-key ctl-x-map [(control ?=)] 'zoom-in/out)
+  (define-key ctl-x-map [(control ?0)] 'zoom-in/out)
+  )
