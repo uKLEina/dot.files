6e19113aaddd8e29fe1b7eed068cd2cdc385d7c5
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2018-06-14 13:00:18 +0900

    slime-mode時に明示的lisp-modeに入るように

--- .emacs.d/inits/50cl.el
+++ .emacs.d/inits/50cl.el
@@ -1,6 +1,8 @@
 (use-package slime
   :config
-  (setq inferior-lisp-program "/usr/bin/sbcl")
-  (slime-setup '(slime-repl slime-fancy))
   (add-to-list 'auto-mode-alist '("\\.lisp\\'" . slime-mode))
+  (add-hook 'slime-mode-hook 'lisp-mode)
+  (setq inferior-lisp-program "/usr/local/bin/clisp")
+  (use-package slime-company)
+  (slime-setup '(slime-repl slime-fancy slime-company))
   )
