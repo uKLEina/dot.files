4297fe25f9dd64595b5ee6f938c74f3a0c7784b6
Naohiro Kobayashi <nkob@n-kobayashi-desktop>
2018-06-15 10:40:17 +0900

    Common Lispの処理系をclispからsbclに変更

--- .emacs.d/inits/50cl.el
+++ .emacs.d/inits/50cl.el
@@ -2,7 +2,7 @@
   :config
   (add-to-list 'auto-mode-alist '("\\.lisp\\'" . slime-mode))
   (add-hook 'slime-mode-hook 'lisp-mode)
-  (setq inferior-lisp-program "/usr/local/bin/clisp")
+  (setq inferior-lisp-program "/usr/bin/sbcl")
   (use-package slime-company)
   (slime-setup '(slime-repl slime-fancy slime-company))
   )
