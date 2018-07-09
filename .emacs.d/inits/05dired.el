eea2f2e5ceb6d954bd40cf8b12f40f17a9cd9684
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2018-07-09 16:49:03 +0900

    diredのomit表示をデフォルトではオフに変更

--- .emacs.d/inits/05dired.el
+++ .emacs.d/inits/05dired.el
@@ -3,7 +3,7 @@
   :config
   (use-package dired-x)
   (setq dired-omit-files (concat dired-omit-files "\\|^\\..+$"))
-  (setq-default dired-omit-files-p t) ; Buffer-local variable
+  (setq-default dired-omit-files-p nil) ; Buffer-local variable
   (bind-key "C-l C-o" 'dired-omit-mode dired-mode-map)
   ;; fix keybind for SKK
   (setq dired-bind-jump nil)
