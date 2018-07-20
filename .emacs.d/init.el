e3a98c665012da1df0890473c4e7ce1a8a3264f7
Naohiro Kobayashi <nkob@n-kobayashi-desktop>
2018-07-20 10:28:55 +0900

    names.elを明示的にインストールするようにする

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -100,7 +100,7 @@
   (el-get-bundle magit))
 (el-get-bundle evil-mc :type git :url "https://github.com/gabesoft/evil-mc.git" :depends (evil))
 (el-get-bundle visual-regexp-steroids)
-
+(el-get-bundle names :type git :url "https://github.com/Malabarba/names.git")
 (el-get-bundle electric-operator :type git :url "https://github.com/davidshepherd7/electric-operator.git")
 
 ;; C/C++
