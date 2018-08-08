4ebc54297119a681c1f4a0fe90b04a796d4e943f
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2018-08-08 09:11:06 +0900

    バッファサイズの変更をsmartrepできるように

--- .emacs.d/inits/30smartrep.el
+++ .emacs.d/inits/30smartrep.el
@@ -2,4 +2,8 @@
   :config
   (smartrep-define-key
       global-map "C-l"
-    '(("<tab>" . tab-to-tab-stop))))
+    '(("<tab>" . tab-to-tab-stop)))
+  (smartrep-define-key
+      global-map "C-x"
+    '(("}" . enlarge-window-horizontally)
+      ("{" . shrink-window-horizontally))))
