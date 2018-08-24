fe63d437a7cfdb49978728dffd20c9c859db303b
Naohiro Kobayashi <nkob@n-kobayashi-desktop>
2018-08-24 15:39:24 +0900

    pandoc-modeをgithubからインストールするようにする

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -151,7 +151,7 @@
 (el-get-bundle markdown-mode)
 (el-get-bundle matlab-mode :type elpa)
 (el-get-bundle graphviz-dot-mode)
-(el-get-bundle pandoc :type elpa)
+(el-get-bundle pandoc-mode)
 
 ;; theme
 (el-get-bundle color-theme-zenburn)
