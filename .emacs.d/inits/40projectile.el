68a5c72fdb405e9eb87cad33c6bd0f7e914d2aa2
Naohiro Kobayashi <nkob@n-kobayashi-desktop>
2018-08-31 18:00:33 +0900

    Projectile-mode有効化キーバインドを追加

--- .emacs.d/inits/40projectile.el
+++ .emacs.d/inits/40projectile.el
@@ -1,10 +1,11 @@
 (use-package projectile
   :defer t
+  :init
+  (bind-key "C-l p e" 'projectile-mode)
   )
 
 (use-package helm-projectile
   :defer t
   :init
-  (bind-key "C-l p f" 'helm-projectile-find-file-dwim)
   (bind-key "C-l p f" 'helm-projectile-find-file-dwim)
   )
