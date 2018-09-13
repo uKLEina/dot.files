b3710473024ffa0d4e4391523dee22322fd2df2f
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2018-09-13 09:42:25 +0900

    zoom-frmを削除したのに設定は残っていたので設定の方も削除

--- .emacs.d/inits/20zoom-frm.el
+++
@@ -1,9 +0,0 @@
-(use-package zoom-frm
-  :defer t
-  :commands (zoom-in/out)
-  :init
-  (define-key ctl-x-map [(control ?+)] 'zoom-in/out)
-  (define-key ctl-x-map [(control ?-)] 'zoom-in/out)
-  (define-key ctl-x-map [(control ?=)] 'zoom-in/out)
-  (define-key ctl-x-map [(control ?0)] 'zoom-in/out)
-  )
