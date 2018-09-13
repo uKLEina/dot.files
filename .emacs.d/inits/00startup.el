131986c92e077d7881872e1fe97b86ca3f5c08e3
Naohiro Kobayashi <nkob@n-kobayashi-desktop>
2018-09-13 09:44:00 +0900

    emacs-serverの設定を微修正

--- .emacs.d/inits/00startup.el
+++ .emacs.d/inits/00startup.el
@@ -1,6 +1,5 @@
 (use-package server
-  :commands (server-running-p)
-  :init (unless (server-running-p)
+  :config (unless (server-running-p)
           (server-start)))
 
 (add-to-list 'load-path (locate-user-emacs-file "elisp"))
