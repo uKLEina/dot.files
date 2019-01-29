c3985322ff8be7f374bb88c27a7f79179cb87de2
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2019-01-29 14:43:15 +0900

    fix yasnippet-snippets setting

--- .emacs.d/inits/40yasnippet.el
+++ .emacs.d/inits/40yasnippet.el
@@ -1,11 +1,13 @@
 (use-package yasnippet
   :defer t
   :diminish yas-minor-mode
+  :commands (yas-expand)
   :init
   (yas-global-mode 1)
   :bind
   (("<C-tab>" . yas-expand))
   :config
+  (use-package yasnippet-snippets)
   (push '("emacs.+/snippets/" . snippet-mode) auto-mode-alist))
 
 (use-package helm-c-yasnippet
