8a3a950b00581935e50ad3ce1c47b6d7fb062a57
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2019-02-07 16:48:26 +0900

    add electric-operator settings for Julia

--- .emacs.d/inits/30electric-operator.el
+++ .emacs.d/inits/30electric-operator.el
@@ -2,4 +2,4 @@
   :diminish
   :init
   (add-hook 'python-mode-hook #'electric-operator-mode)
-  )
+  (add-hook 'ess-julia-mode-hook #'electric-operator-mode))
--- .emacs.d/inits/50julia.el
+++ .emacs.d/inits/50julia.el
@@ -1,7 +1,81 @@
 (use-package ess-julia
   :mode (("\\.jl\\'" . ess-julia-mode))
-  :init
-  (add-hook 'ess-julia-mode-hook #'auto-complete-mode)
   :config
-  (company-mode -1)
+  (use-package electric-operator)
+  (apply #'electric-operator-add-rules-for-mode 'ess-julia-mode (electric-operator-get-rules-for-mode 'prog-mode))
+  (electric-operator-add-rules-for-mode 'ess-julia-mode
+                                        (cons "=" #'electric-operator-julia-mode-kwargs-=)
+                                        (cons ";" "; ")
+
+                                        ;; Subtype comparison
+                                        (cons "<:" " <: ")
+
+                                        ;; Cool! Unicode!
+                                        (cons "÷" " ÷ ")
+                                        (cons "≠" " ≠ ")
+                                        (cons "≤" " ≤ ")
+                                        (cons "≥" " ≥ ")
+
+                                        ;; something about fractions
+                                        (cons "//" " // ")
+                                        (cons ".//" " .// ")
+                                        (cons "//=" " //= ")
+
+                                        ;; pipe
+                                        (cons "|>" " |> ")
+
+                                        (cons "*" " * ")
+                                        (cons "/" " / ")
+                                        (cons "%" " % ")
+                                        (cons "&" " & ")
+
+                                        ;; \ (escaped), for solving matrix multiplies
+                                        (cons "\\" " \\ ")
+                                        (cons "\\=" " \\= ")
+                                        (cons ".\\" " .\\ ")
+
+                                        ;; XOR
+                                        (cons "$" " $ ")
+
+                                        ;; Even more equal!
+                                        (cons "===" " === ")
+                                        (cons "!==" " !== ")
+
+                                        ;; vector operations and assign-operators
+                                        (cons ".^" " .^ ")
+                                        (cons ".*" " .* ")
+                                        (cons "./" " ./ ")
+                                        (cons ".%" " .% ")
+                                        (cons "<<" " << ")
+                                        (cons ">>" " >> ")
+                                        (cons ">>>" " >>> ")
+                                        (cons ".<<" " .<< ")
+                                        (cons ".>>" " .>> ")
+                                        (cons ".>>>" " .>>> ")
+                                        (cons ".+" " .+ ")
+                                        (cons ".-" " .- ")
+                                        (cons ".>" " .> ")
+                                        (cons ".<" " .< ")
+                                        (cons ".>=" " .>= ")
+                                        (cons ".<=" " .<= ")
+                                        (cons ".==" " .== ")
+                                        (cons ".!=" " .!= ")
+                                        (cons "^=" " ^= ")
+                                        (cons "÷=" " ÷= ")
+                                        (cons "%=" " %= ")
+                                        (cons "|=" " |= ")
+                                        (cons "&=" " &= ")
+                                        (cons "$=" " $= ")
+                                        (cons "<<=" " <<= ")
+                                        (cons ">>=" " >>= ")
+                                        (cons ">>>=" " >>>= ")
+                                        (cons ".+=" " .+= ")
+                                        (cons ".-=" " .-= ")
+                                        (cons ".*=" " .*= ")
+                                        (cons "./=" " ./= ")
+                                        (cons ".//=" " .//= ")
+                                        (cons ".\\=" " .\\= ")
+                                        (cons ".^=" " .^= ")
+                                        (cons ".÷=" " .÷= ")
+                                        (cons ".%=" " .%= "))
   )
