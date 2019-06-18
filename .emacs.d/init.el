ec9f0e116d887d3ff3d6e95b5c56091c2abe559e
Naohiro Kobayashi <n.kobayashi@pc-link.co.jp>
2019-06-18 12:15:16 +0900

    delete unnecessory comments

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -288,35 +288,7 @@
   :config
   (setq doom-modeline-minor-modes t)
   (setq doom-modeline-major-mode-color-icon t)
-  (setq doom-modeline-checker-simple-format nil)
-  ;; (setq doom-modeline-bar-width 10)
-  ;; mode-line color as evil state
-  ;; normal: BG-Alt
-  ;; (add-hook 'evil-normal-state-entry-hook
-  ;;           (lambda ()
-  ;;             (set-face-background 'mode-line (doom-color 'bg-alt))))
-  ;; ;; insert: Green
-  ;; (add-hook 'evil-insert-state-entry-hook
-  ;;           (lambda ()
-  ;;             (set-face-background 'mode-line (doom-darken (doom-color 'green) 0.5))))
-  ;; (add-hook 'evil-insert-state-exit-hook
-  ;;           (lambda ()
-  ;;             (set-face-background 'mode-line (doom-color 'bg-alt))))
-  ;; ;; visual: Blue
-  ;; (add-hook 'evil-visual-state-entry-hook
-  ;;           (lambda ()
-  ;;             (set-face-background 'mode-line (doom-color 'dark-blue))))
-  ;; (add-hook 'evil-visual-state-exit-hook
-  ;;           (lambda ()
-  ;;             (set-face-background 'mode-line (doom-color 'bg-alt))))
-  ;; ;; emacs: Red
-  ;; (add-hook 'evil-emacs-state-entry-hook
-  ;;           (lambda ()
-  ;;             (set-face-background 'mode-line (doom-color 'magenta))))
-  ;; (add-hook 'evil-emacs-state-exit-hook
-  ;;           (lambda ()
-  ;;             (set-face-background 'mode-line (doom-color 'bg-alt))))
-  )
+  (setq doom-modeline-checker-simple-format nil))
 
 (add-to-list 'load-path "~/.emacs.d/elisp")
 (use-package evil-mode-line
