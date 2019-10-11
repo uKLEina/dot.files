b21e1559fe7848023f985c147fe1d2273a44e8c0
Naohiro Kobayashi <n.kobayashi@sinops.jp>
2019-10-11 11:12:25 +0900

    minor fix for display management scripts

--- bin/highdpi.sh
+++ bin/highdpi.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xrandr --output eDP-1 --mode 2560x1440 --scale 1.375x1.375 --panning 3520x1980
+xrandr --output eDP-1 --mode 2560x1440 --scale 1.375x1.375 --panning 3520x1980 --fb 3520x1980
--- bin/right.sh
+++ bin/right.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 out_w=`expr $1 \* 2`
 out_h=`expr $2 \* 2`
-xrandr --output eDP-1 --scale 1.25x1.25 --mode 2560x1440 --panning 3200x1800 --output HDMI-2 --scale 2x2 --mode $1x$2 --panning ${out_w}x${out_h} --right-of eDP-1
+size_w=`expr 2560 \+ $1 \* 2`
+# xrandr --output eDP-1 --scale 1.25x1.25 --mode 2560x1440 --panning 3200x1800 --output HDMI-2 --scale 1x1 --mode $1x$2 --panning ${out_w}x${out_h}+3200+0 # --right-of eDP-1
+xrandr --output eDP-1 --scale 1.25x1.25 --mode 2560x1440 --panning 3200x1800 --pos 0x0 --output HDMI-2 --scale 2x2 --mode $1x$2 --panning ${out_w}x${out_h}+3200+0 --pos 3200x0 --fb ${size_w}x1800
