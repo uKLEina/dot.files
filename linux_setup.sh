1106350917348fb9e4633b66d72fcc1a1a0250e5
Naohiro Kobayashi <n.kobayashi@sinops.jp>
2019-12-29 18:21:36 +0900

    minor fix

--- linux_setup.sh
+++ linux_setup.sh
@@ -19,7 +19,6 @@ rm -f ~/.zshrc
 rm -f ~/.zshenv
 rm -f ~/hhkb.nodoka
 rm -f ~/nihongo.nodoka
-rm -fr ~/.config/alacritty
 
 # .config/
 mkdir -p ~/.config
@@ -27,7 +26,6 @@ ln -s ~/dot.files/.config/pip ~/.config/pip
 ln -s ~/dot.files/.config/flake8 ~/.config/flake8
 ln -s ~/dot.files/.config/pep8 ~/.config/pep8
 ln -s ~/dot.files/.config/pylintrc ~/.config/pylintrc
-ln -s ~/dot.files/.config/alacritty ~/.config/alacritty
 ln -s ~/dot.files/.gitignore_global ~/.gitignore_global
 
 # Emacs
