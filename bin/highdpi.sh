30a0e00d170a676f4bfa13433f19252da06fd16d
Naohiro Kobayashi <n.kobayashi@sinops.jp>
2020-04-16 17:21:34 +0900

    fix highdpi settings

--- bin/highdpi.sh
+++ bin/highdpi.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xrandr --output eDP-1 --mode 2560x1440 --scale 1.25x1.25 --panning 3200x1800 --fb 3200x1800
+xrandr --output eDP-1 --mode 2560x1440 --scale 1.25x1.25 --panning 3200x1800+3840+0 --right-of HDMI-2 --output HDMI-2 --mode 3840x2160 --scale 1x1 --panning 3840x2160+0+0
