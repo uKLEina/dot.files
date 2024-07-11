48492ae7617769053114020751a6d676915ac8cb
Naohiro Kobayashi <n.kobayashi@sinops.jp>
2024-07-11 20:42:48 +0900

    disable cl-funstions warning suppression

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -2,3 +2,6 @@
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars . nil) default-frame-alist)
 (push '(alpha-background . 90) default-frame-alist)
+
+;; (customize-set-variable 'byte-compile-warnings '(cl-functions))
+;; (setopt warning-suppress-types '((package cl)))
