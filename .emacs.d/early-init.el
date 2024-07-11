030910d90bce83c53aca4b021fd3e55ac62b1735
Naohiro Kobayashi <n.kobayashi@sinops.jp>
2024-07-11 19:01:57 +0900

    add transparency setting to early-init

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -1,3 +1,4 @@
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars . nil) default-frame-alist)
+(push '(alpha-background . 90) default-frame-alist)
