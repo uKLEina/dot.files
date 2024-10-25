61dcc656a19cf24af5fa838b76392092e8dd564c
Kobayashi Naohiro <n.kobayashi@sinops.jp>
2024-10-25 11:12:27 +0900

    delete unused org-mode settings. now just keybinds

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1300,42 +1300,10 @@ frame if FRAME is nil, and to 1 if AMT is nil."
 ;;   (turn-on-reftex)
 
 (use-package org
-  :defer t
-  :init
-  ;; reftex with org mode
-  (add-hook 'org-mode-hook 'turn-on-reftex)
-  :config
-  (defun org-mode-reftex-setup ()
-    (load-library "reftex")
-    (and (buffer-file-name)
-         (file-exists-p (buffer-file-name))
-         (reftex-parse-all))
-    (define-key org-mode-map (kbd "C-c [") 'reftex-citation))
-  ;; Org Mode LaTeX Export
-  (use-package ox-latex)
-  (use-package ox-beamer)
-  (unless (boundp 'org-latex-classes)
-    (setq org-latex-classes nil))
-  (setq org-latex-pdf-process '("latexmk %f"))
-  (setq org-latex-default-class "bxjsarticle")
-  ;; org-latex-classes
-  (add-to-list 'org-latex-classes
-               '("bxjsarticle"
-                 "\\documentclass[autodetect-engine,dvipdfmx-if-dvi,ja=standard,a4paper,12pt]{bxjsarticle}"
-                 ("\\section{%s}" . "\\section*{%s}")
-                 ("\\subsection{%s}" . "\\subsection*{%s}")
-                 ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-                 ("\\paragraph{%s}" . "\\paragraph*{%s}")
-                 ("\\subparagraph{%s}" . "\\subparagraph*{%s}")
-                 ))
-  (custom-set-variables '(org-latex-classes (delete (assoc "beamer" org-latex-classes) org-latex-classes)))
-  (add-to-list 'org-latex-classes
-               '("beamer"
-                 "\\documentclass[dvipdfmx,presentation]{beamer}
-                 [NO-DEFAULT-PACKAGES] [PACKAGES] [EXTRA]"
-                 ("\\section\{%s\}" . "\\section*\{%s\}")
-                 ("\\subsection\{%s\}" . "\\subsection*\{%s\}")
-                 ("\\subsubsection\{%s\}" . "\\subsubsection*\{%s\}"))))
+  :bind
+  ("C-l C-o l" . org-store-link)
+  ("C-l C-o a" . org-agenda)
+  ("C-l C-o c" . org-capture))
 
 (use-package vimrc-mode
   :ensure t
