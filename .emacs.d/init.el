c28e3929dbfd7ec381023bde069ba2b428202362
Kobayashi Naohiro <n.kobayashi@sinops.jp>
2025-07-01 19:02:43 +0900

    add neotree options

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1359,6 +1359,12 @@ Uses explorer.exe for WSL with properly escaped paths and nautilus for non-WSL."
   (evil-set-initial-state 'neotree-mode 'emacs)
   :custom
   (neo-theme 'nerd-icons)
+  (neo-smart-open t)
+  (neo-force-change-root t)
+  (neo-autorefresh t)
+  (neo-window-fixed-size nil)
+  (neo-show-hidden-files t)
+  ;; (neo-vc-integration '(char))
   :config
   (bind-keys :map neotree-mode-map
              ("j" . neotree-next-line)
