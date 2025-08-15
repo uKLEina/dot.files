608ee93da067a3154ba2909f5207ea16022d0f34
Kobayashi Naohiro <n.kobayashi@sinops.jp>
2025-08-15 16:54:03 +0900

    comment out old lines in linux_setup.sh

--- linux_setup.sh
+++ linux_setup.sh
@@ -21,29 +21,29 @@ rm -f ~/hhkb.nodoka
 rm -f ~/nihongo.nodoka
 
 # .config/
-mkdir -p ~/.config
-ln -s ~/dot.files/.config/pip ~/.config/pip
-ln -s ~/dot.files/.config/flake8 ~/.config/flake8
-ln -s ~/dot.files/.config/pep8 ~/.config/pep8
-ln -s ~/dot.files/.config/pylintrc ~/.config/pylintrc
+# mkdir -p ~/.config
+# ln -s ~/dot.files/.config/pip ~/.config/pip
+# ln -s ~/dot.files/.config/flake8 ~/.config/flake8
+# ln -s ~/dot.files/.config/pep8 ~/.config/pep8
+# ln -s ~/dot.files/.config/pylintrc ~/.config/pylintrc
 ln -s ~/dot.files/.gitignore_global ~/.gitignore_global
-ln -s ~/dot.files/nihongo.kanata.lisp ~/.config/nihongo.kanata.lisp
+# ln -s ~/dot.files/nihongo.kanata.lisp ~/.config/nihongo.kanata.lisp
 
 # Emacs
 ln -s ~/dot.files/.emacs.d ~/.emacs.d
 ln -s ~/dot.files/.skk.d ~/.skk.d
 
 # themes
-mkdir ~/.themes
-ln -s ~/dot.files/.themes/Numix ~/.themes/Numix
+# mkdir ~/.themes
+# ln -s ~/dot.files/.themes/Numix ~/.themes/Numix
 
 # shell
 ln -s ~/dot.files/.zsh ~/.zsh
 ln -s ~/dot.files/.zshrc ~/.zshrc
 ln -s ~/dot.files/.zshenv ~/.zshenv
-touch .bash_profile
-echo "export PATH=\"$HOME/.cargo/bin:\$PATH\"" >> ~/.bash_profile
-echo "export PATH=\"$HOME/.cargo/bin:\$PATH\"" >> ~/.profile
+# touch .bash_profile
+# echo "export PATH=\"$HOME/.cargo/bin:\$PATH\"" >> ~/.bash_profile
+# echo "export PATH=\"$HOME/.cargo/bin:\$PATH\"" >> ~/.profile
 ln -s ~/dot.files/.tmux.conf ~/.tmux.conf
 
 # scripts
@@ -56,5 +56,5 @@ git config --global alias.ci commit
 git config --global alias.st status
 
 # others
-ln -s ~/dot.files/.aspell.conf ~/.aspell.conf
-ln -s ~/dot.files/.latexmkrc ~/.latexmkrc
+# ln -s ~/dot.files/.aspell.conf ~/.aspell.conf
+# ln -s ~/dot.files/.latexmkrc ~/.latexmkrc
