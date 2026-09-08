cd18cc0a51737b6cfef2889a60eb01e51e62aea2
Kobayashi Naohiro <kobayashi.naohiro@classmethod.jp>
2026-09-08 10:07:11 +0900

    perf(consult): 非同期検索の開始文字数を2文字に設定
    - 短い入力での不要な検索を抑え、検索時の負荷を軽減

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -977,6 +977,8 @@ For visual-char ('v') or visual-block ('C-v'), places cursors at the column."
    ("C-M-g" . consult-ripgrep)
    ("M-g g" . consult-goto-line)
    )
+  :custom
+  (consult-async-min-input 2)
   :config
   ;; cosult-line-thing-at-point
   (consult-customize consult-line :add-history (seq-some #'thing-at-point '(region symbol)))
